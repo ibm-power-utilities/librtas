94efe8cb14637820af35bba555997d5bd5f92704
Thomas Falcon <tlfalcon@linux.vnet.ibm.com>
2015-12-15 13:25:40 -0500

    librtas: common.h merged into librtas.h
    Code in common.h was added to librtas.h
    
    Signed-off-by: Thomas Falcon <tlfalcon@linux.vnet.ibm.com>

--- librtas_src/librtas.h
+++ librtas_src/librtas.h
@@ -10,6 +10,8 @@
 #ifndef _LIBRTAS_H_
 #define _LIBRTAS_H_
 
+#include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <stdint.h>
 
@@ -23,16 +25,16 @@
 #define RTAS_IO_ASSERT	-1098	/* Unexpected I/O Error */
 #define RTAS_UNKNOWN_OP -1099	/* No Firmware Implementation of Function */
 
+#define RC_BUSY -2
+#define EXTENDED_DELAY_MIN 9900
+#define EXTENDED_DELAY_MAX 9905
+
+#define PAGE_SIZE 4096
+
 /* Adding a new RTAS call requires the following:
  * 1) A function prototype in librtas.h (this file) that roughly matches
  *    the RTAS call name.
- * 2) An implementation of the new function in common.c.  Use the existing
- *    ones as an example.
- * 3) A function pointer definition in struct rtas_operations in common.h.
- *    Prepend a "token" argument to the argument list.
- * 4) An implementation-specific version of the RTAS function, in 
- *    syscall_calls.c and/or procfs_calls.c.  Remember to add your new 
- *    function to the rtas_operations struct at the top of the file.
+ * 2) An implementation of the RTAS function, in librtas_calls.c.
  */
 
 #ifdef __cplusplus
@@ -48,7 +50,8 @@ extern int rtas_errinjct(int etoken, int otoken, char *workarea);
 extern int rtas_errinjct_close(int otoken);
 extern int rtas_errinjct_open(int *otoken);
 extern int rtas_free_rmo_buffer(void *buf, uint32_t phys_addr, size_t size);
-extern int rtas_get_config_addr_info2(uint32_t cfg_addr, uint64_t phb_id, uint32_t func, uint32_t *info);
+extern int rtas_get_config_addr_info2(uint32_t cfg_addr, uint64_t phb_id,
+				uint32_t func, uint32_t *info);
 extern int rtas_get_dynamic_sensor(int sensor, void *loc_code, int *state);
 extern int rtas_get_indices(int is_sensor, int type, char *workarea,
 				size_t size, int start, int *next);
@@ -93,5 +96,36 @@ extern int rtas_update_properties(char *workarea, unsigned int scope);
 #ifdef __cplusplus
 }
 #endif
-	
+
+struct librtas_config {
+	uint64_t rtas_timeout_ms;
+	int debug;
+};
+
+extern int open_proc_rtas_file(const char *name, int mode);
+extern int rtas_token(const char *call_name);
+extern int read_entire_file(int fd, char **buf, size_t *len);
+
+extern struct librtas_config config;
+
+#define dbg(_lvl, _fmt, _args...) 					\
+	do {								\
+		if (config.debug >= _lvl) 				\
+			printf("librtas %s(): " _fmt, __FUNCTION__, ##_args); \
+	} while (0)
+
+#define dbg1(_fmt, _args...)						\
+	do {								\
+		dbg(1, _fmt, ##_args);					\
+	} while (0)							\
+
+#define SANITY_CHECKS()                                         \
+        /* Check credentials */                                 \
+        if (geteuid() != (uid_t) 0)                             \
+                return RTAS_PERM;                               \
+                                                                \
+        /* Check for any kernel RTAS interface */               \
+        if (!interface_exists())                                 \
+                return RTAS_KERNEL_INT;
+
 #endif /* _LIBRTAS_H_ */
