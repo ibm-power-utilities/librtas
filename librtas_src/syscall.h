ibm-power-utilities/librtas

commit eae18de31155d109a0160485ee7d93bbdb32d2e0

librtas: Update the dbg() macro
Remove the useless indirect of dbg1() macro and move the definition
to an internal .h file.

Signed-off-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>

librtas_src/syscall.h
@@ -44,4 +44,10 @@ extern int interface_exists();
 #define BITS32_HI(_num) (uint32_t) (_num >> 32) 
 #define BITS64(_high, _low) (uint64_t) (((uint64_t) _high << 32) | _low)
 
+#define dbg(_fmt, _args...)						  \
+	do {								  \
+		if (config.debug >= 0)					  \
+			printf("librtas %s(): " _fmt, __func__, ##_args); \
+	} while (0)
+
 #endif

librtas_src/librtas.h
@@ -108,17 +108,6 @@ extern int read_entire_file(int fd, char **buf, size_t *len);
 
 extern struct librtas_config config;
 
-#define dbg(_lvl, _fmt, _args...) 					\
-	do {								\
-		if (config.debug >= _lvl) 				\
-			printf("librtas %s(): " _fmt, __FUNCTION__, ##_args); \
-	} while (0)
-
-#define dbg1(_fmt, _args...)						\
-	do {								\
-		dbg(1, _fmt, ##_args);					\
-	} while (0)							\
-
 #define SANITY_CHECKS()                                         \
         /* Check credentials */                                 \
         if (geteuid() != (uid_t) 0)                             \

librtas_src/ofdt.c
@@ -14,6 +14,7 @@
 #include <endian.h>
 #include <byteswap.h>
 #include "librtas.h"
+#include "syscall.h"
 
 static const char *ofdt_rtas_path = "/proc/device-tree/rtas";
 
@@ -137,7 +138,7 @@ int read_entire_file(int fd, char **buf, size_t * len)
 
 		rc = read(fd, *buf + off, BLOCK_SIZE);
 		if (rc < 0) {
-			dbg1("read failed\n");
+			dbg("read failed\n");
 			errno = EIO;
 			return -1;
 		}

librtas_src/syscall_calls.c
@@ -71,7 +71,7 @@ static unsigned int handle_delay(int status, uint64_t * elapsed)
 	}
 	*elapsed += ms;
 
-	dbg1("Return status %d, delaying for %ld ms\n", status, ms);
+	dbg("Return status %d, delaying for %ld ms\n", status, ms);
 	usleep(ms * 1000);
 	return 1;
 }
@@ -157,7 +157,7 @@ static int _rtas_call(int delay_handling, int token, int ninputs,
 	} while (rc == CALL_AGAIN);
 
 	if (rc != 0) {
-		dbg1("RTAS syscall failure, errno=%d\n", errno);
+		dbg("RTAS syscall failure, errno=%d\n", errno);
 		return RTAS_IO_ASSERT;
 	}
 	display_rtas_buf(&args, 1);
@@ -219,7 +219,7 @@ int rtas_activate_firmware()
 
 	rc = rtas_call("ibm,activate-firmware", 0, 1, &status);
 
-	dbg1("() = %d\n", rc ? rc : status);
+	dbg("() = %d\n", rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -274,7 +274,7 @@ int rtas_cfg_connector(char *workarea)
 	if (extent_pa)
 		(void)rtas_free_rmo_buffer(extent, extent_pa, PAGE_SIZE);
 
-	dbg1("(%p) = %d\n", workarea, rc ? rc : status);
+	dbg("(%p) = %d\n", workarea, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -307,7 +307,7 @@ int rtas_display_char(char c)
 
 	rc = rtas_call("display-character", 1, 1, c, &status);
 
-	dbg1("(%d) = %d\n", c, rc ? rc : status);
+	dbg("(%d) = %d\n", c, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -338,7 +338,7 @@ int rtas_display_msg(char *buf)
 		       &status);
 	(void)rtas_free_rmo_buffer(kernbuf, kernbuf_pa, str_len);
 
-	dbg1("(%p) = %d\n", buf, rc ? rc : status);
+	dbg("(%p) = %d\n", buf, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -374,7 +374,7 @@ int rtas_errinjct(int etoken, int otoken, char *workarea)
 
 	(void)rtas_free_rmo_buffer(kernbuf, kernbuf_pa, ERRINJCT_BUF_SIZE);
 
-	dbg1("(%d, %d, %p) = %d\n", etoken, otoken, workarea, rc ? rc : status);
+	dbg("(%d, %d, %p) = %d\n", etoken, otoken, workarea, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -392,7 +392,7 @@ int rtas_errinjct_close(int otoken)
 
 	rc = rtas_call("ibm,close-errinjct", 1, 1, htobe32(otoken), &status);
 
-	dbg1("(%d) = %d\n", otoken, rc ? rc : status);
+	dbg("(%d) = %d\n", otoken, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -415,7 +415,7 @@ int rtas_errinjct_open(int *otoken)
 	rc = rtas_call("ibm,open-errinjct", 0, 2, &be_otoken, &status);
 	*otoken = be32toh(be_otoken);
 
-	dbg1("(%p) = %d, %d\n", otoken, rc ? rc : status, *otoken);
+	dbg("(%p) = %d, %d\n", otoken, rc ? rc : status, *otoken);
 	return rc ? rc : status;
 }
 
@@ -485,8 +485,8 @@ int rtas_get_dynamic_sensor(int sensor, void *loc_code, int *state)
 
 	*state = be32toh(be_state);
 
-	dbg1("(%d, %s, %p) = %d, %d\n", sensor, (char *)loc_code, state,
-	     rc ? rc : status, *state);
+	dbg("(%d, %s, %p) = %d, %d\n", sensor, (char *)loc_code, state,
+	    rc ? rc : status, *state);
 	return rc ? rc : status;
 }
 
@@ -554,8 +554,8 @@ int rtas_get_power_level(int powerdomain, int *level)
 
 	*level = be32toh(be_level);
 
-	dbg1("(%d, %p) = %d, %d\n", powerdomain, level, rc ? rc : status,
-	     *level);
+	dbg("(%d, %p) = %d, %d\n", powerdomain, level, rc ? rc : status,
+	    *level);
 	return rc ? rc : status;
 }
 
@@ -582,8 +582,8 @@ int rtas_get_sensor(int sensor, int index, int *state)
 
 	*state = be32toh(be_state);
 
-	dbg1("(%d, %d, %p) = %d, %d\n", sensor, index, state, rc ? rc : status,
-	     *state);
+	dbg("(%d, %d, %p) = %d, %d\n", sensor, index, state, rc ? rc : status,
+	    *state);
 	return rc ? rc : status;
 }
 
@@ -620,7 +620,7 @@ rtas_get_sysparm(unsigned int parameter, unsigned int length,
 
 	(void)rtas_free_rmo_buffer(kernbuf, kernbuf_pa, length);
 
-	dbg1("(%d, %d, %p) = %d\n", parameter, length, data, rc ? rc : status);
+	dbg("(%d, %d, %p) = %d\n", parameter, length, data, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -657,8 +657,8 @@ int rtas_get_time(uint32_t *year, uint32_t *month, uint32_t *day,
 	*sec = be32toh(*sec);
 	*nsec = be32toh(*nsec);
 
-	dbg1("() = %d, %d, %d, %d, %d, %d, %d, %d\n", rc ? rc : status, *year,
-			*month, *day, *hour, *min, *sec, *nsec);
+	dbg("() = %d, %d, %d, %d, %d, %d, %d, %d\n", rc ? rc : status, *year,
+	    *month, *day, *hour, *min, *sec, *nsec);
 	return rc ? rc : status;
 }
 
@@ -772,8 +772,8 @@ int rtas_lpar_perftools(int subfunc, char *workarea,
 
 	*seq_next = be32toh(*seq_next);
 
-	dbg1("(%d, %p, %d, %d, %p) = %d, %d\n", subfunc, workarea,
-	     length, sequence, seq_next, rc ? rc : status, *seq_next);
+	dbg("(%d, %p, %d, %d, %p) = %d, %d\n", subfunc, workarea,
+	    length, sequence, seq_next, rc ? rc : status, *seq_next);
 	return rc ? rc : status;
 }
 /**
@@ -952,8 +952,8 @@ int rtas_set_dynamic_indicator(int indicator, int new_value, void *loc_code)
 
 	(void) rtas_free_rmo_buffer(locbuf, loc_pa, size);
 
-	dbg1("(%d, %d, %s) = %d\n", indicator, new_value, (char *)loc_code,
-	     rc ? rc : status);
+	dbg("(%d, %d, %s) = %d\n", indicator, new_value, (char *)loc_code,
+	    rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -997,8 +997,8 @@ int rtas_set_indicator(int indicator, int index, int new_value)
 	rc = rtas_call("set-indicator", 3, 1, htobe32(indicator),
 		       htobe32(index), htobe32(new_value), &status);
 
-	dbg1("(%d, %d, %d) = %d\n", indicator, index, new_value,
-	     rc ? rc : status);
+	dbg("(%d, %d, %d) = %d\n", indicator, index, new_value,
+	    rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -1022,8 +1022,8 @@ int rtas_set_power_level(int powerdomain, int level, int *setlevel)
 
 	*setlevel = be32toh(be_setlevel);
 
-	dbg1("(%d, %d, %p) = %d, %d\n", powerdomain, level, setlevel,
-	     rc ? rc : status, *setlevel);
+	dbg("(%d, %d, %p) = %d, %d\n", powerdomain, level, setlevel,
+	    rc ? rc : status, *setlevel);
 	return rc ? rc : status;
 }
 
@@ -1051,8 +1051,8 @@ rtas_set_poweron_time(uint32_t year, uint32_t month, uint32_t day,
 		       htobe32(month), htobe32(day), htobe32(hour),
 		       htobe32(min), htobe32(sec), htobe32(nsec), &status);
 
-	dbg1("(%d, %d, %d, %d, %d, %d, %d) = %d\n", year, month, day, hour,
-	     min, sec, nsec, rc ? rc : status);
+	dbg("(%d, %d, %d, %d, %d, %d, %d) = %d\n", year, month, day, hour,
+	    min, sec, nsec, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -1084,7 +1084,7 @@ int rtas_set_sysparm(unsigned int parameter, char *data)
 
 	(void)rtas_free_rmo_buffer(kernbuf, kernbuf_pa, size + sizeof(short));
 
-	dbg1("(%d, %p) = %d\n", parameter, data, rc ? rc : status);
+	dbg("(%d, %p) = %d\n", parameter, data, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -1111,8 +1111,8 @@ int rtas_set_time(uint32_t year, uint32_t month, uint32_t day,
 		       htobe32(day), htobe32(hour), htobe32(min),
 		       htobe32(sec), htobe32(nsec), &status);
 
-	dbg1("(%d, %d, %d, %d, %d, %d, %d) = %d\n", year, month, day, hour,
-			min, sec, nsec, rc ? rc : status);
+	dbg("(%d, %d, %d, %d, %d, %d, %d) = %d\n", year, month, day, hour,
+	    min, sec, nsec, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -1129,7 +1129,7 @@ int rtas_suspend_me(uint64_t streamid)
 	rc = rtas_call("ibm,suspend-me", 2, 1, htobe32(BITS32_HI(streamid)),
 		       htobe32(BITS32_LO(streamid)), &status);
 
-	dbg1("() = %d\n", rc ? rc : status);
+	dbg("() = %d\n", rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -1165,7 +1165,7 @@ int rtas_update_nodes(char *workarea, unsigned int scope)
 
 	(void)rtas_free_rmo_buffer(kernbuf, workarea_pa, PAGE_SIZE);
 
-	dbg1("(%p) %d = %d\n", workarea, scope, rc ? rc : status);
+	dbg("(%p) %d = %d\n", workarea, scope, rc ? rc : status);
 	return rc ? rc : status;
 }
 
@@ -1201,6 +1201,6 @@ int rtas_update_properties(char *workarea, unsigned int scope)
 
 	(void)rtas_free_rmo_buffer(kernbuf, workarea_pa, PAGE_SIZE);
 
-	dbg1("(%p) %d = %d\n", workarea, scope, rc ? rc : status);
+	dbg("(%p) %d = %d\n", workarea, scope, rc ? rc : status);
 	return rc ? rc : status;
 }

librtas_src/syscall_rmo.c
@@ -60,7 +60,7 @@ int open_proc_rtas_file(const char *name, int mode)
 	}
 
 	if (fd < 0)
-		dbg1("Failed to open %s\n", full_name);
+		dbg("Failed to open %s\n", full_name);
 
 	return fd;
 }
@@ -80,7 +80,7 @@ static int read_kregion_bounds(struct region *kregion)
 
 	fd = open_proc_rtas_file(rmo_filename, O_RDONLY);
 	if (fd < 0) {
-		dbg1("Could not open workarea file\n");
+		dbg("Could not open workarea file\n");
 		return RTAS_IO_ASSERT;
 	}
 
@@ -96,7 +96,7 @@ static int read_kregion_bounds(struct region *kregion)
 
 	if (!(kregion->size && kregion->addr) ||
 	    (kregion->size > (PAGE_SIZE * MAX_PAGES))) {
-		dbg1("Unexpected kregion bounds\n");
+		dbg("Unexpected kregion bounds\n");
 		return RTAS_IO_ASSERT;
 	}
 
@@ -155,7 +155,7 @@ static int acquire_file_lock(off_t start, size_t size)
 		wa_config.lockfile_fd = open(lockfile_path, O_CREAT | O_RDWR,
 					     S_IRUSR | S_IWUSR);
 		if (wa_config.lockfile_fd < 0) {
-			dbg1("could not open lockfile %s\n", lockfile_path);
+			dbg("could not open lockfile %s\n", lockfile_path);
 			return RTAS_IO_ASSERT;
 		}
 	}
@@ -169,7 +169,7 @@ static int acquire_file_lock(off_t start, size_t size)
 	rc = fcntl(wa_config.lockfile_fd, F_SETLKW, &flock);
 	if (rc < 0) {
 		/* Expected to fail for regions used by other processes */
-		dbg1("fcntl failed for [0x%lx, 0x%x]\n", start, size);
+		dbg("fcntl failed for [0x%lx, 0x%x]\n", start, size);
 		return RTAS_IO_ASSERT;
 	}
 
@@ -196,7 +196,7 @@ static int release_file_lock(off_t start, size_t size)
 
 	rc = fcntl(wa_config.lockfile_fd, F_SETLK, &flock);
 	if (rc < 0) {
-		dbg1("fcntl failed for [0x%lx, 0x%x]\n", start, size);
+		dbg("fcntl failed for [0x%lx, 0x%x]\n", start, size);
 		return RTAS_IO_ASSERT;
 	}
 
@@ -219,7 +219,7 @@ static int get_phys_region(size_t size, uint32_t * phys_addr)
 	int i;
 
 	if (size > kregion->size) {
-		dbg1("Invalid buffer size 0x%x requested\n", size);
+		dbg("Invalid buffer size 0x%x requested\n", size);
 		return RTAS_IO_ASSERT;
 	}
 
@@ -242,7 +242,7 @@ static int get_phys_region(size_t size, uint32_t * phys_addr)
 	}
 
 	if (!addr) {
-		dbg1("Could not find available workarea space\n");
+		dbg("Could not find available workarea space\n");
 		return RTAS_IO_ASSERT;
 	}
 
@@ -266,7 +266,7 @@ static int release_phys_region(uint32_t phys_addr, size_t size)
 	int rc;
 
 	if (size > kregion->size) {
-		dbg1("Invalid buffer size 0x%x requested\n", size);
+		dbg("Invalid buffer size 0x%x requested\n", size);
 		return RTAS_IO_ASSERT;
 	}
 
@@ -276,7 +276,7 @@ static int release_phys_region(uint32_t phys_addr, size_t size)
 	bits =
 	    get_bits(first_page, first_page + n_pages - 1, wa_config.pages_map);
 	if (bits != ((1 << n_pages) - 1)) {
-		dbg1("Invalid region [0x%x, 0x%x]\n", phys_addr, size);
+		dbg("Invalid region [0x%x, 0x%x]\n", phys_addr, size);
 		return RTAS_IO_ASSERT;
 	}
 
@@ -321,7 +321,7 @@ static int mmap_dev_mem(uint32_t phys_addr, size_t size, void **buf)
 
 	fd = open(devmem_path, O_RDWR);
 	if (fd < 0) {
-		dbg1("Failed to open %s\n", devmem_path);
+		dbg("Failed to open %s\n", devmem_path);
 		return RTAS_IO_ASSERT;
 	}
 
@@ -330,7 +330,7 @@ static int mmap_dev_mem(uint32_t phys_addr, size_t size, void **buf)
 	close(fd);
 
 	if (newbuf == MAP_FAILED) {
-		dbg1("mmap failed\n");
+		dbg("mmap failed\n");
 		return RTAS_IO_ASSERT;
 	}
 
@@ -352,14 +352,14 @@ static int munmap_dev_mem(void *buf, size_t size)
 
 	fd = open(devmem_path, O_RDWR);
 	if (fd < 0) {
-		dbg1("Failed to open %s\n", devmem_path);
+		dbg("Failed to open %s\n", devmem_path);
 		return RTAS_IO_ASSERT;
 	}
 
 	rc = munmap(buf, size);
 	close(fd);
 	if (rc < 0) {
-		dbg1("munmap failed\n");
+		dbg("munmap failed\n");
 		return RTAS_IO_ASSERT;
 	}
 
@@ -411,7 +411,7 @@ int rtas_free_rmo_buffer(void *buf, uint32_t phys_addr, size_t size)
 	}
 
 	if (!wa_config.init_done) {
-		dbg1("Attempting to free before calling get()\n");
+		dbg("Attempting to free before calling get()\n");
 		return RTAS_FREE_ERR;
 	}
 
@@ -447,7 +447,7 @@ int rtas_get_rmo_buffer(size_t size, void **buf, uint32_t * phys_addr)
 	int n_pages;
 	int rc;
 
-	dbg1("RMO buffer request, size: %d\n", size);
+	dbg("RMO buffer request, size: %d\n", size);
 
 	n_pages = size / PAGE_SIZE;
 

commit af07fde662985cb574fe56497847c4c6cdf1d1a8

librtas: Do not enable debug message by default
Commit eae18de3 removed dbg1 macro. But accidently enabled logging debug
message by default. This is confusing end users. Hence remove logging
debug message by default.

Fixes: eae18de3 (librtas: Update the dbg() macro)
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
CC: Nathan Fontenot <nfont@linux.vnet.ibm.com>

librtas_src/syscall.h
@@ -64,7 +64,7 @@ extern int dbg_lvl;
 
 #define dbg(_fmt, _args...)						  \
 	do {								  \
-		if (dbg_lvl >= 0)					  \
+		if (dbg_lvl > 0)					  \
 			printf("librtas %s(): " _fmt, __func__, ##_args); \
 	} while (0)
 