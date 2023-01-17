753a2e01dbd2f9da7fcf45302e23d5f73312d522
Nathan Lynch <nathanl@linux.ibm.com>
2023-01-17 13:14:29 -0600

    librtas.h: drop extern from function declarations
    Reduce clutter in librtas.h by removing all the unnecessary
    extern's. No functional change.
    
    Signed-off-by: Nathan Lynch <nathanl@linux.ibm.com>
    Signed-off-by: Tyrel Datwyler <tyreld@linux.ibm.com>

--- librtas_src/librtas.h
+++ librtas_src/librtas.h
@@ -52,59 +52,52 @@
 extern "C" {
 #endif
 
-extern int rtas_activate_firmware();
-extern int rtas_cfg_connector(char *workarea);
-extern int rtas_delay_timeout(uint64_t timeout_ms);
-extern int rtas_display_char(char c);
-extern int rtas_display_msg(char *buf);
-extern int rtas_errinjct(int etoken, int otoken, char *workarea);
-extern int rtas_errinjct_close(int otoken);
-extern int rtas_errinjct_open(int *otoken);
-extern int rtas_free_rmo_buffer(void *buf, uint32_t phys_addr, size_t size);
-extern int rtas_get_config_addr_info2(uint32_t cfg_addr, uint64_t phb_id,
-				uint32_t func, uint32_t *info);
-extern int rtas_get_dynamic_sensor(int sensor, void *loc_code, int *state);
-extern int rtas_get_indices(int is_sensor, int type, char *workarea,
-				size_t size, int start, int *next);
-extern int rtas_get_power_level(int powerdomain, int *level);
-extern int rtas_get_rmo_buffer(size_t size, void **buf, uint32_t *phys_addr);
-extern int rtas_get_sensor(int sensor, int index, int *state);
-extern int rtas_get_sysparm(unsigned int parameter, unsigned int length,
-				char *data);
-extern int rtas_get_time(uint32_t *year, uint32_t *month, uint32_t *day,
-				uint32_t *hour, uint32_t *min, uint32_t *sec, 
-				uint32_t *nsec);
-extern int rtas_get_vpd(char *loc_code, char *workarea, size_t size, 
-				unsigned int sequence, unsigned int *seq_next,
-				unsigned int *bytes_ret);
-extern int rtas_lpar_perftools(int subfunc, char *workarea, 
-				unsigned int length, unsigned int sequence, 
-				unsigned int *seq_next);
-extern int rtas_platform_dump(uint64_t dump_tag, uint64_t sequence, 
-				void *buffer, size_t length, 
-				uint64_t *next_seq, uint64_t *bytes_ret);
-extern int rtas_read_slot_reset(uint32_t cfg_addr, uint64_t phbid, int *state, 
-				int *eeh);
-extern int rtas_scan_log_dump(void *buffer, size_t length);
-extern int rtas_set_debug(int level);
-extern int rtas_set_dynamic_indicator(int indicator, int new_value,
-				void *loc_code);
-extern int rtas_set_eeh_option(uint32_t cfg_addr, uint64_t phbid, 
-				int function);
-extern int rtas_set_indicator(int indicator, int index, int new_value);
-extern int rtas_set_power_level(int powerdomain, int level, int *setlevel);
-extern int rtas_set_poweron_time(uint32_t year, uint32_t month, uint32_t day,
-				uint32_t hour, uint32_t min, uint32_t sec, 
-				uint32_t nsec);
-extern int rtas_set_sysparm(unsigned int parameter, char *data);
-extern int rtas_set_time(uint32_t year, uint32_t month, uint32_t day, 
-				uint32_t hour, uint32_t min, uint32_t sec, 
-				uint32_t nsec);
-extern int rtas_suspend_me(uint64_t streamid);
-extern int rtas_update_nodes(char *workarea, unsigned int scope);
-extern int rtas_update_properties(char *workarea, unsigned int scope);
-extern int rtas_physical_attestation(char *workarea, int seq_num,
-				     int *next_seq_num, int *work_area_bytes);
+int rtas_activate_firmware();
+int rtas_cfg_connector(char *workarea);
+int rtas_delay_timeout(uint64_t timeout_ms);
+int rtas_display_char(char c);
+int rtas_display_msg(char *buf);
+int rtas_errinjct(int etoken, int otoken, char *workarea);
+int rtas_errinjct_close(int otoken);
+int rtas_errinjct_open(int *otoken);
+int rtas_free_rmo_buffer(void *buf, uint32_t phys_addr, size_t size);
+int rtas_get_config_addr_info2(uint32_t cfg_addr, uint64_t phb_id,
+			       uint32_t func, uint32_t *info);
+int rtas_get_dynamic_sensor(int sensor, void *loc_code, int *state);
+int rtas_get_indices(int is_sensor, int type, char *workarea,
+		     size_t size, int start, int *next);
+int rtas_get_power_level(int powerdomain, int *level);
+int rtas_get_rmo_buffer(size_t size, void **buf, uint32_t *phys_addr);
+int rtas_get_sensor(int sensor, int index, int *state);
+int rtas_get_sysparm(unsigned int parameter, unsigned int length, char *data);
+int rtas_get_time(uint32_t *year, uint32_t *month, uint32_t *day,
+		  uint32_t *hour, uint32_t *min, uint32_t *sec, uint32_t *nsec);
+int rtas_get_vpd(char *loc_code, char *workarea, size_t size,
+		 unsigned int sequence, unsigned int *seq_next,
+		 unsigned int *bytes_ret);
+int rtas_lpar_perftools(int subfunc, char *workarea,
+			unsigned int length, unsigned int sequence,
+			unsigned int *seq_next);
+int rtas_platform_dump(uint64_t dump_tag, uint64_t sequence,
+		       void *buffer, size_t length,
+		       uint64_t *next_seq, uint64_t *bytes_ret);
+int rtas_read_slot_reset(uint32_t cfg_addr, uint64_t phbid, int *state, int *eeh);
+int rtas_scan_log_dump(void *buffer, size_t length);
+int rtas_set_debug(int level);
+int rtas_set_dynamic_indicator(int indicator, int new_value, void *loc_code);
+int rtas_set_eeh_option(uint32_t cfg_addr, uint64_t phbid, int function);
+int rtas_set_indicator(int indicator, int index, int new_value);
+int rtas_set_power_level(int powerdomain, int level, int *setlevel);
+int rtas_set_poweron_time(uint32_t year, uint32_t month, uint32_t day,
+			  uint32_t hour, uint32_t min, uint32_t sec, uint32_t nsec);
+int rtas_set_sysparm(unsigned int parameter, char *data);
+int rtas_set_time(uint32_t year, uint32_t month, uint32_t day,
+		  uint32_t hour, uint32_t min, uint32_t sec, uint32_t nsec);
+int rtas_suspend_me(uint64_t streamid);
+int rtas_update_nodes(char *workarea, unsigned int scope);
+int rtas_update_properties(char *workarea, unsigned int scope);
+int rtas_physical_attestation(char *workarea, int seq_num,
+			      int *next_seq_num, int *work_area_bytes);
 
 #ifdef __cplusplus
 }
